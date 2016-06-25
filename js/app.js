06b5ead6d58709e440b15f84d5f430ea8c55fc3b
Chris Johnson <csjohnson79@icloud.com>
2016-06-25 17:04:52 -0600

    Took away one possible answer from HTML, created and defined the global variables.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,53 @@
-$
+$(document).ready(function () {
+	
+
+
+/*----DEFINE THE GLOBAL VARIABLES----*/	
+	var questionsArray [
+
+		{
+			question: 'Approximately what year did construction of the famous Protestant church Grossmunster begin?',
+			answerChoices: ['1100 A.D.', '1396 B.C.', '2016 A.D.', '0 B.C.', 'THE YEAR OF THE MONKEY'],
+			correctAnswer: 0
+		},
+
+		{
+			question: 'What famous structure in Zurich sports the largest clock face in Europe?',
+			answerChoices: ['St. Peter\'s Church', 'Fraumunster', 'Grossmunster', 'Stadthaus'],
+			correctAnswer: 1
+		},
+
+		{
+			question: 'What famous artist created the 5 stained glass windows in the Fraumunster Cathedral?',
+			answerChoices: ['Andy Warhol', 'Claude Monet', 'Marc Chagall', 'Salvador Dali'],
+			correctAnswer: 2
+		},
+
+		{
+			question: 'Into which body of water does the Limmat River flow into?',
+			answerChoices: ['The Mediterranean Sea', 'The Atlantic Ocean', 'The Dead Sea', 'Lake Zurich'],
+			correctAnswer: 3
+		},
+
+		{
+			question: 'What is the population of Zurich?',
+			answerChoices: ['378,884', '1,655,470', '13,000,000', '5,666,889'],
+			correctAnswer: 0
+		}
+
+	];
+
+	var currentQuestionNumber = 0;
+	var totalNumberOfQuestions = 5;
+	var numberOfCorrectAnswers = 0;
+
+
+/*----DEFINE THE FUNCTIONS----*/	
+
+	function questionStats() {
+
+		$
+	}
+
+
+}
